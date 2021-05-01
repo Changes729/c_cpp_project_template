e90877b7797d39b632c6c85753c126ce0f6f260b
Asuki <changes729@163.com>
2021-05-01 20:15:43 +0800

    update io-flush example

--- spike/src/io-flush-main.c
+++ spike/src/io-flush-main.c
@@ -1,23 +1,32 @@
 #include <stdio.h>
+#include <string.h>
 #include <unistd.h>
 
 #include "io-flush.h"
 
+static bool _quit = false;
+
 static void stdin_callback(void* user_data, fd_desc_t pkg)
 {
   char buffer[256] = {};
-  read(pkg.fd, &buffer, sizeof(buffer));
+  int  result      = read(pkg.fd, &buffer, sizeof(buffer));
 
-  printf("%s\n", buffer);
+  if(result > 0) {
+    printf("%s\n", buffer);
 
-  io_ignore_file(pkg.fd);
+    if(strncmp("quit", buffer, strlen("quit")) == 0) {
+      _quit = true;
+
+      io_ignore_file(0);
+    }
+  }
 }
 
 int main(void)
 {
   io_notice_file((fd_desc_t){0, IO_NOTICE_READ}, stdin_callback, NULL);
 
-  while(1) {
+  while(!_quit) {
     io_flush_select();
   }
 
