26b18319135f75b24bf645b1fc1d3d96f186fca9
步丶丶 <changes729@163.com>
2021-05-02 14:22:13 +0800

    implement a module for io flush. (#14)
    * implement a module for io flush.
    
    * fix cmake workflow error. (#15) (#16)
    
    * fix cmake workflow error.
    
    * add libdbus-1 for workflow
    
    * add libdbus-glib-1 for workflow
    
    * add sub module for workflow
    
    * add libasound2 for workflow
    
    * add example for io-flush select.
    
    * update io-flush example
    
    * add poll epoll support

---
+++ spike/shared/lib/io-flush.h
@@ -0,0 +1,45 @@
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef _IO_FLUSH_H
+#define _IO_FLUSH_H
+#pragma once
+/* Public include ------------------------------------------------------------*/
+#include <stdbool.h>
+
+/* Public namespace ----------------------------------------------------------*/
+#ifdef __cplusplus
+extern "C" {
+#endif
+/* Public define -------------------------------------------------------------*/
+/* Public typedef ------------------------------------------------------------*/
+enum io_notice_type {
+  IO_NOTICE_READ  = 1 << 0,
+  IO_NOTICE_WRITE = 1 << 1,
+  IO_NOTICE_ERR   = 1 << 2,
+  IO_NOTICE_HUP   = 1 << 3,
+};
+
+/* Public template -----------------------------------------------------------*/
+typedef struct
+{
+  int   fd;
+  short flag;
+} fd_desc_t;
+
+typedef void (*fd_callback_t)(void* user_data, fd_desc_t pkg);
+
+/* Public function prototypes ------------------------------------------------*/
+/** select, poll ---------------------------------------------------*/
+bool io_epoll_fd_init();
+void io_epoll_fd_deinit();
+
+void io_flush_select(/*timeout*/);
+void io_flush_poll(/*timeout*/);
+void io_flush_epoll(/*timeout*/);
+
+bool io_notice_file(fd_desc_t pkg, fd_callback_t callback, void* user_data);
+void io_ignore_file(int fd);
+
+#ifdef __cplusplus
+}
+#endif
+#endif /* _IO_FOCUS_H */
