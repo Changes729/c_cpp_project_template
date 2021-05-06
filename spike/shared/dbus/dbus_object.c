400d494c598f1fe879dcc9f7e82ab44cfd9f1b55
Asuki <changes729@163.com>
2021-05-06 11:25:06 +0800

    add support for object manager interface.

--- spike/shared/dbus/dbus_object.c
+++ spike/shared/dbus/dbus_object.c
@@ -23,6 +23,16 @@ find_interface_method(DBusMessage *, struct dbus_object *);
 
 static DBusMessage *introspect(DBusConnection *, DBusMessage *, void *user_data);
 
+static int append_object(void *data, void *user_data);
+static DBusMessage *
+get_objects(DBusConnection *connection, DBusMessage *message, void *user_data);
+static void append_interfaces(struct dbus_object *data, DBusMessageIter *iter);
+static int  append_interface(void *data, void *user_data);
+
+static struct dbus_object *
+invalidate_parent_data(DBusConnection *conn, const char *child_path);
+static void emit_interfaces_added(struct dbus_object *data);
+
 /* Private variables ---------------------------------------------------------*/
 static DBusMethodTable introspect_methods[] = {
     {DBUS_METHOD("Introspect", NULL, DBUS_ARGS({"xml", "s"}), introspect)},
@@ -57,7 +67,7 @@ static DBusMethodTable object_methods[] = {
     {DBUS_METHOD("GetManagedObjects",
                  NULL,
                  DBUS_ARGS({"objects", "a{oa{sa{sv}}}"}),
-                 NULL)},
+                 get_objects)},
     {},
 };
 
@@ -105,6 +115,9 @@ void register_dbus_object_path(DBusConnection *conn)
                 NULL,
                 data,
                 NULL);
+
+  attach_dbus_object(conn, "/org/bluez");
+  attach_dbus_object(conn, "/org/bluez/hci0");
 }
 
 void unregister_dbus_object_path(DBusConnection *conn)
@@ -115,6 +128,9 @@ void unregister_dbus_object_path(DBusConnection *conn)
   {
     return;
   }
+
+  detach_dbus_object(conn, "/org/bluez/hci0");
+  detach_dbus_object(conn, "/org/bluez/");
 
   remove_interface(dbus_object, DBUS_INTERFACE_INTROSPECTABLE);
   remove_interface(dbus_object, DBUS_INTERFACE_PROPERTIES);
@@ -153,7 +169,7 @@ dbus_object_t *attach_dbus_object(DBusConnection *conn, const char *path)
   if(dbus_connection_register_object_path(conn, path, &server_vtable, dbus_object) ==
      TRUE)
   {
-    // todo: invalidate_parent_data(connection, path);
+    invalidate_parent_data(conn, path);
     if(add_interface(dbus_object,
                      DBUS_INTERFACE_INTROSPECTABLE,
                      introspect_methods,
@@ -190,7 +206,7 @@ void detach_dbus_object(DBusConnection *conn, const char *path)
   sets_cleanup(&dbus_object->removed);
   remove_interface(dbus_object, DBUS_INTERFACE_INTROSPECTABLE);
   // todo: notify PropertiesChanged
-  // todo: invalidate_parent_data(dbus_object->conn, dbus_object->path);
+  invalidate_parent_data(conn, path);
 
   dbus_connection_unregister_object_path(dbus_object->conn, dbus_object->path);
 
@@ -398,4 +414,285 @@ introspect(DBusConnection *connection, DBusMessage *message, void *user_data)
 
 end:
   return reply;
+}
+
+static bool check_experimental(int flags, int flag)
+{
+  static int global_flags = 0;
+  if(!(flags & flag)) return FALSE;
+
+  return !(global_flags & G_DBUS_FLAG_ENABLE_EXPERIMENTAL);
+}
+
+static void append_property(struct interface_data *  iface,
+                            const DBusPropertyTable *p,
+                            DBusMessageIter *        dict)
+{
+  DBusMessageIter entry, value;
+
+  dbus_message_iter_open_container(dict, DBUS_TYPE_DICT_ENTRY, NULL, &entry);
+  dbus_message_iter_append_basic(&entry, DBUS_TYPE_STRING, &p->name);
+  dbus_message_iter_open_container(&entry, DBUS_TYPE_VARIANT, p->type, &value);
+
+  p->get(p, &value, iface->user_data);
+
+  dbus_message_iter_close_container(&entry, &value);
+  dbus_message_iter_close_container(dict, &entry);
+}
+
+static void append_properties(struct interface_data *data, DBusMessageIter *iter)
+{
+  DBusMessageIter          dict;
+  const DBusPropertyTable *p;
+
+  // clang-format off
+  dbus_message_iter_open_container(iter, DBUS_TYPE_ARRAY,
+          DBUS_DICT_ENTRY_BEGIN_CHAR_AS_STRING
+          DBUS_TYPE_STRING_AS_STRING
+          DBUS_TYPE_VARIANT_AS_STRING
+          DBUS_DICT_ENTRY_END_CHAR_AS_STRING,
+          &dict);
+  // clang-format on
+
+  for(p = data->properties; p && p->name; p++) {
+    if(check_experimental(p->flags, G_DBUS_PROPERTY_FLAG_EXPERIMENTAL))
+      continue;
+
+    if(p->get == NULL) continue;
+
+    if(p->exists != NULL && !p->exists(p, data->user_data)) continue;
+
+    append_property(data, p, &dict);
+  }
+
+  dbus_message_iter_close_container(iter, &dict);
+}
+
+static int append_interface(void *data, void *user_data)
+{
+  struct interface_data *iface = data;
+  DBusMessageIter *      array = user_data;
+  DBusMessageIter        entry;
+
+  dbus_message_iter_open_container(array, DBUS_TYPE_DICT_ENTRY, NULL, &entry);
+  dbus_message_iter_append_basic(&entry, DBUS_TYPE_STRING, &iface->name);
+  append_properties(data, &entry);
+  dbus_message_iter_close_container(array, &entry);
+
+  return true;
+}
+
+static void append_interfaces(struct dbus_object *data, DBusMessageIter *iter)
+{
+  DBusMessageIter array;
+
+  // clang-format off
+  dbus_message_iter_open_container(iter, DBUS_TYPE_ARRAY,
+          DBUS_DICT_ENTRY_BEGIN_CHAR_AS_STRING
+          DBUS_TYPE_STRING_AS_STRING
+          DBUS_TYPE_ARRAY_AS_STRING
+          DBUS_DICT_ENTRY_BEGIN_CHAR_AS_STRING
+          DBUS_TYPE_STRING_AS_STRING
+          DBUS_TYPE_VARIANT_AS_STRING
+          DBUS_DICT_ENTRY_END_CHAR_AS_STRING
+          DBUS_DICT_ENTRY_END_CHAR_AS_STRING,
+          &array);
+  // clang-format on
+
+  sets_foreach(&data->interfaces, append_interface, &array);
+
+  dbus_message_iter_close_container(iter, &array);
+}
+
+static int append_object(void *data, void *user_data)
+{
+  struct dbus_object *child = data;
+  DBusMessageIter *   array = user_data;
+  DBusMessageIter     entry;
+
+  dbus_message_iter_open_container(array, DBUS_TYPE_DICT_ENTRY, NULL, &entry);
+  dbus_message_iter_append_basic(&entry, DBUS_TYPE_OBJECT_PATH, &child->path);
+  append_interfaces(child, &entry);
+  dbus_message_iter_close_container(array, &entry);
+
+  sets_foreach(&child->objects, append_object, user_data);
+
+  return true;
+}
+
+static DBusMessage *
+get_objects(DBusConnection *connection, DBusMessage *message, void *user_data)
+{
+  struct dbus_object *data = user_data;
+  DBusMessage *       reply;
+  DBusMessageIter     iter;
+  DBusMessageIter     array;
+
+  reply = dbus_message_new_method_return(message);
+  if(reply == NULL) return NULL;
+
+  dbus_message_iter_init_append(reply, &iter);
+
+  // clang-format off
+	dbus_message_iter_open_container(&iter, DBUS_TYPE_ARRAY,
+					DBUS_DICT_ENTRY_BEGIN_CHAR_AS_STRING
+					DBUS_TYPE_OBJECT_PATH_AS_STRING
+					DBUS_TYPE_ARRAY_AS_STRING
+					DBUS_DICT_ENTRY_BEGIN_CHAR_AS_STRING
+					DBUS_TYPE_STRING_AS_STRING
+					DBUS_TYPE_ARRAY_AS_STRING
+					DBUS_DICT_ENTRY_BEGIN_CHAR_AS_STRING
+					DBUS_TYPE_STRING_AS_STRING
+					DBUS_TYPE_VARIANT_AS_STRING
+					DBUS_DICT_ENTRY_END_CHAR_AS_STRING
+					DBUS_DICT_ENTRY_END_CHAR_AS_STRING
+					DBUS_DICT_ENTRY_END_CHAR_AS_STRING,
+					&array);
+  // clang-format on
+
+  sets_foreach(&data->objects, append_object, &array);
+
+  dbus_message_iter_close_container(&iter, &array);
+
+  return reply;
+}
+
+static struct dbus_object *
+invalidate_parent_data(DBusConnection *conn, const char *child_path)
+{
+  struct dbus_object *data = NULL, *child = NULL, *parent = NULL;
+  char *              parent_path, *slash;
+
+  parent_path = strdup(child_path);
+  slash       = strrchr(parent_path, '/');
+  if(slash == NULL) goto done;
+
+  if(slash == parent_path && parent_path[1] != '\0')
+    parent_path[1] = '\0';
+  else
+    *slash = '\0';
+
+  if(!strlen(parent_path)) goto done;
+
+  if(dbus_connection_get_object_path_data(conn, parent_path, (void *)&data) ==
+     FALSE)
+  {
+    goto done;
+  }
+
+  parent = invalidate_parent_data(conn, parent_path);
+
+  if(data == NULL) {
+    data = parent;
+    if(data == NULL) goto done;
+  }
+
+  free(data->introspect);
+  data->introspect = NULL;
+
+  if(!dbus_connection_get_object_path_data(conn, child_path, (void *)&child))
+    goto done;
+
+  if(child == NULL || sets_find(&data->objects, child, NULL)) goto done;
+
+  sets_add(&data->objects, child);
+  child->parent = data;
+
+done:
+  free(parent_path);
+  return data;
+}
+
+static int append_name(void *data, void *user_data)
+{
+  char *           name = data;
+  DBusMessageIter *iter = user_data;
+
+  dbus_message_iter_append_basic(iter, DBUS_TYPE_STRING, &name);
+
+  return true;
+}
+
+static void emit_interfaces_added(struct dbus_object *data)
+{
+  DBusMessage *   signal;
+  DBusMessageIter iter, array;
+
+  if(strcmp(data->path, root.path) == 0) return;
+
+  signal = dbus_message_new_signal(root.path,
+                                   DBUS_INTERFACE_OBJECT_MANAGER,
+                                   "InterfacesAdded");
+  if(signal == NULL) return;
+
+  dbus_message_iter_init_append(signal, &iter);
+  dbus_message_iter_append_basic(&iter, DBUS_TYPE_OBJECT_PATH, &data->path);
+
+  // clang-format off
+	dbus_message_iter_open_container(&iter, DBUS_TYPE_ARRAY,
+				DBUS_DICT_ENTRY_BEGIN_CHAR_AS_STRING
+				DBUS_TYPE_STRING_AS_STRING
+				DBUS_TYPE_ARRAY_AS_STRING
+				DBUS_DICT_ENTRY_BEGIN_CHAR_AS_STRING
+				DBUS_TYPE_STRING_AS_STRING
+				DBUS_TYPE_VARIANT_AS_STRING
+				DBUS_DICT_ENTRY_END_CHAR_AS_STRING
+				DBUS_DICT_ENTRY_END_CHAR_AS_STRING, &array);
+  // clang-format on
+
+  sets_foreach(&data->added, append_interface, &array);
+  sets_cleanup(&data->added);
+
+  dbus_message_iter_close_container(&iter, &array);
+
+  /* Use dbus_connection_send to avoid recursive calls to g_dbus_flush */
+  dbus_connection_send(data->conn, signal, NULL);
+  dbus_message_unref(signal);
+}
+
+static void emit_interfaces_removed(struct dbus_object *data)
+{
+  DBusMessage *   signal;
+  DBusMessageIter iter, array;
+
+  if(strcmp(data->path, root.path) == 0) return;
+
+  signal = dbus_message_new_signal(root.path,
+                                   DBUS_INTERFACE_OBJECT_MANAGER,
+                                   "InterfacesRemoved");
+  if(signal == NULL) return;
+
+  dbus_message_iter_init_append(signal, &iter);
+  dbus_message_iter_append_basic(&iter, DBUS_TYPE_OBJECT_PATH, &data->path);
+  dbus_message_iter_open_container(&iter,
+                                   DBUS_TYPE_ARRAY,
+                                   DBUS_TYPE_STRING_AS_STRING,
+                                   &array);
+
+  sets_foreach(&data->removed, append_name, &array);
+  sets_cleanup(&data->removed);
+
+  dbus_message_iter_close_container(&iter, &array);
+
+  /* Use dbus_connection_send to avoid recursive calls to g_dbus_flush */
+  dbus_connection_send(data->conn, signal, NULL);
+  dbus_message_unref(signal);
+}
+
+static bool process_changes(void *user_data)
+{
+  struct dbus_object *data = user_data;
+
+  remove_pending(data);
+
+  if(sets_length(&data->added) > 0) emit_interfaces_added(data);
+
+  /* Flush pending properties */
+  if(data->pending_prop == TRUE) process_property_changes(data);
+
+  if(sets_length(&data->removed) > 0) emit_interfaces_removed(data);
+
+  // data->process_id = 0;
+
+  return FALSE;
 }
