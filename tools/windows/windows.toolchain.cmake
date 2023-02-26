ce13e83eabbce2fe0d5fa1f2ca57e79ba6f8f55a
Changes729 <changes0729@163.com>
2023-02-26 09:21:01 +0800

    add threadpool and update tools.

--- tools/windows/windows.toolchain.cmake
+++ tools/windows/windows.toolchain.cmake
@@ -2,11 +2,15 @@
 SET(CMAKE_SYSTEM_NAME Windows)
 
 # specify the cross compiler
-SET(CMAKE_C_COMPILER   /usr/bin/x86_64-w64-mingw32-gcc)
-SET(CMAKE_CXX_COMPILER /usr/bin/x86_64-w64-mingw32-g++)
-
-# where is the target environment
-SET(CMAKE_FIND_ROOT_PATH  /usr/x86_64-w64-mingw32/)
+if(APPLE)
+  SET(CMAKE_C_COMPILER      /opt/homebrew/bin/x86_64-w64-mingw32-gcc)
+  SET(CMAKE_CXX_COMPILER    /opt/homebrew/bin/x86_64-w64-mingw32-g++)
+  SET(CMAKE_FIND_ROOT_PATH  /opt/homebrew/Cellar/mingw-w64/10.0.0_4/toolchain-x86_64/x86_64-w64-mingw32/)
+else(LINUX)
+  SET(CMAKE_C_COMPILER      /usr/bin/x86_64-w64-mingw32-gcc)
+  SET(CMAKE_CXX_COMPILER    /usr/bin/x86_64-w64-mingw32-g++)
+  SET(CMAKE_FIND_ROOT_PATH  /usr/x86_64-w64-mingw32/)
+endif()
 
 # search for programs in the build host directories (not necessary)
 SET(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
